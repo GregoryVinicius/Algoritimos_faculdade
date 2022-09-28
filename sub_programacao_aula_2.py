cacd871661ec104774ff681054c47d3323f7eb0c
GregoryVinicius <gregoryviniciuscla@gmail.com>
2022-09-27 21:50:30 -0300

    Subprogramação

--- sub_programacao_aula_2.py
+++ sub_programacao_aula_2.py
@@ -3,9 +3,6 @@ Exercicio
 criar um sub-algoritimo chamado fatorial
 """
 
-"""
-
-
 def fatorial(x1):
     resultado = 1
     for i in range (x1,1,-1):
@@ -35,7 +32,7 @@ x = 10
 print(x)
 mostraX()
 print(x)
-"""
+
 
 def fatorial (fat):
     resultado = 1
@@ -45,3 +42,19 @@ def fatorial (fat):
     return resultado
 
 print(fatorial(5))
+
+
+def menor (v):
+    menor = 100000000
+    for i in range (0, len(v)):
+        if (v[i] < menor):
+            menor = v[i]
+    return menor
+
+v = [0] * 10
+
+for i in range (0, len(v)):
+    v[i] = int (input ("insira um valor: "))
+
+x = menor(v)
+print(menor(v))
